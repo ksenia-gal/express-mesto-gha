7a9b8c29a2bbb8552962d78c7f35a3cc9c4413fb
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-07-14 18:57:31 +0300

    13

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/router');
+
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 
+// создание приложения
 const app = express();
+
 app.use(express.json());
-app.use(routes);
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '64afed73b4096ee42aca255e', // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: '64afed73b4096ee42aca255e',
   };
-
   next();
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  // useNewUrlParser: true,
-});
+app.use(routes);
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.listen(PORT, () => {
   // если всё работает, консоль покажет, какой порт приложение слушает
