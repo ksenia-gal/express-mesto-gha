177fff95e0479d636bde5cfb11a69e19f92946e7
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-07-14 17:41:27 +0300

    13

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 });
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
+  // useNewUrlParser: true,
 });
 
 app.listen(PORT, () => {
