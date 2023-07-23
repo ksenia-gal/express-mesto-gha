e7c5f63838de1c43c051eebd62aff6ed1a29005b
Ksenia Galkina <k5eniiagalkina@tandex.ru>
2023-07-23 14:45:37 +0300

    14 project

--- controllers/users.js
+++ controllers/users.js
@@ -9,23 +9,42 @@ const ConflictError = require('../errors/conflictError');
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
-  return User.findUserByCredentials(email, password)
+  User.findUserByCredentials(email, password)
     .then((user) => {
-      // создадим токен
       const token = jwt.sign(
         { _id: user._id },
         NODE_ENV === 'production' ? JWT_SECRET : 'secret-key',
         { expiresIn: '7d' },
       );
-        // вернем токен
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-      });
-      res.send({ token });
+      res
+        .cookie('token', token, {
+          maxAge: 3600000 * 24 * 7,
+          // httpOnly: true,
+        })
+        .send({ token });
     })
     .catch(next);
 };
+
+// const login = (req, res, next) => {
+//   const { email, password } = req.body;
+//   return User.findUserByCredentials(email, password)
+//     .then((user) => {
+//       // создадим токен
+//       const token = jwt.sign(
+//         { _id: user._id },
+//         NODE_ENV === 'production' ? JWT_SECRET : 'secret-key',
+//         { expiresIn: '7d' },
+//       );
+//         // вернем токен
+//       res.cookie('jwt', token, {
+//         maxAge: 3600000 * 24 * 7,
+//         httpOnly: true,
+//       });
+//       res.send({ token });
+//     })
+//     .catch(next);
+// };
 
 // получение всех пользователей из базы данных +
 const getUsers = (req, res, next) => {
